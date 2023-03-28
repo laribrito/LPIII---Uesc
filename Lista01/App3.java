5b730ad9d0091a95c1f4cfbd9333d6a45f8d8432
laribrito <lary.29.ds@gmail.com>
2023-03-28 08:26:58 -0300

    feat: adiciona número aleatório ao crabs

--- Lista01/App3.java
+++ Lista01/App3.java
@@ -1,22 +1,21 @@
 package Lista01;
-import java.util.Scanner;
+import java.util.Random;
 
 // jogo do crabs
 public class App3 {
 
     public static void main(String[] args) {
         //variaveis e objetos
+        Random random = new Random();
         int dado1, dado2, soma, ponto;
         short count;
 
-        Scanner input = new Scanner(System.in);
-
         //primeiro estagio
         System.out.println("Estágio 1:");
-        System.out.print("\nDado1: ");
-        dado1 = input.nextInt();
-        System.out.print("Dado2: ");
-        dado2 = input.nextInt();
+        dado1 = random.nextInt(7);
+        System.out.print("\nDado1: " + dado1);
+        dado2 = random.nextInt(7);
+        System.out.print("\tDado2: "+dado2);
 
         soma = dado1 + dado2;
         if (soma == 7 && soma == 11) {
@@ -31,10 +30,10 @@ public class App3 {
             System.out.print("Ponto: " + ponto);
             count = 1;
             do{
-                System.out.print("\nDado1: ");
-                dado1 = input.nextInt();
-                System.out.print("Dado2: ");
-                dado2 = input.nextInt();
+                dado1 = random.nextInt(7);
+                System.out.print("\nDado1: " + dado1);
+                dado2 = random.nextInt(7);
+                System.out.println("\tDado2: "+dado2);
                 soma = dado1+dado2;
                 System.out.println("Jogada "+ count + ": " + soma);
 
@@ -47,7 +46,5 @@ public class App3 {
                 }
             }while(soma != ponto && soma !=7);
         }
-
-        input.close();
     }
 }
